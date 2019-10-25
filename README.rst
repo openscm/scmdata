5facb27e004cb61b1c838ecf974ec728b08825e0
Jared Lewis <jared@jared.kiwi.nz>
2019-10-25 15:02:18 +1100

    Initial commit
    Used znicholls/example as a starting point

---
+++ README.rst
@@ -0,0 +1,52 @@
+SCMData
+=======
+
+.. sec-begin-index
+
++-------------------+----------------+-----------+-----------+
+| Repository health | |Build Status| | |Codecov| | |License| |
++-------------------+----------------+-----------+-----------+
+
++-----------------+------------------+
+| Latest releases | |Latest Version| |
++-----------------+------------------+
+
++-----------------+----------------+---------------+
+| Latest activity | |Contributors| | |Last Commit| |
++-----------------+----------------+---------------+
+
+SCMData provides some useful data handling routines for dealing with data pertaining to Simple Climate Models (SCMs).
+
+.. sec-end-index
+
+Contributing
+------------
+
+If you'd like to contribute, please make a pull request!
+The pull request templates should ensure that you provide all the necessary information.
+
+.. sec-begin-license
+
+License
+-------
+
+ScmData is free software under a BSD 3-Clause License, see `LICENSE <https://github.com/lewisjared/license/blob/master/LICENSE>`_.
+
+.. sec-end-license
+
+.. sec-begin-links
+
+.. |Build Status| image:: https://travis-ci.com/lewisjared/scmdata.svg?branch=master
+    :target: https://travis-ci.com/lewisjared/scmdata
+.. |Codecov| image:: https://img.shields.io/codecov/c/github/lewisjared/scmdata.svg
+    :target: https://codecov.io/gh/lewisjared/scmdata/branch/master/graph/badge.svg
+.. |License| image:: https://img.shields.io/github/license/lewisjared/scmdata.svg
+    :target: https://github.com/lewisjared/scmdata/blob/master/LICENSE
+.. |Latest Version| image:: https://img.shields.io/github/tag/lewisjared/scmdata.svg
+    :target: https://github.com/lewisjared/scmdata/releases
+.. |Last Commit| image:: https://img.shields.io/github/last-commit/lewisjared/scmdata.svg
+    :target: https://github.com/lewisjared/scmdata/commits/master
+.. |Contributors| image:: https://img.shields.io/github/contributors/lewisjared/scmdata.svg
+    :target: https://github.com/lewisjared/scmdata/graphs/contributors
+
+.. sec-end-links
---
+++ setup.py
@@ -0,0 +1,84 @@
+import versioneer
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+PACKAGE_NAME = "scmdata"
+AUTHOR = "Jared Lewis"
+EMAIL = "jared.lewis@climate-energy-college.org"
+URL = "https://github.com/lewisjared/scmdata"
+
+DESCRIPTION = (
+    "Simple data handling for Simple Climate Model data"
+)
+README = "README.rst"
+
+SOURCE_DIR = "src"
+
+REQUIREMENTS = ["numpy", "scipy"]
+REQUIREMENTS_TESTS = ["codecov", "pytest-cov", "pytest>=4.0"]
+REQUIREMENTS_DOCS = ["sphinx>=1.4", "sphinx_rtd_theme"]
+REQUIREMENTS_DEPLOY = ["twine>=1.11.0", "setuptools>=38.6.0", "wheel>=0.31.0"]
+
+requirements_dev = [
+    *["flake8"],
+    *REQUIREMENTS_TESTS,
+    *REQUIREMENTS_DOCS,
+    *REQUIREMENTS_DEPLOY,
+]
+
+requirements_extras = {
+    "docs": REQUIREMENTS_DOCS,
+    "tests": REQUIREMENTS_TESTS,
+    "deploy": REQUIREMENTS_DEPLOY,
+    "dev": requirements_dev,
+}
+
+with open(README, "r") as readme_file:
+    README_TEXT = readme_file.read()
+
+
+class ScmData(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        pytest.main(self.test_args)
+
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass.update({"test": ScmData})
+
+setup(
+    name=PACKAGE_NAME,
+    version=versioneer.get_version(),
+    description=DESCRIPTION,
+    long_description=README_TEXT,
+    long_description_content_type="text/x-rst",
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    license="3-Clause BSD License",
+    classifiers=[  # full list at https://pypi.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords=["data", "simple climate model", "climate", "scm"],
+    packages=find_packages(SOURCE_DIR),  # no exclude as only searching in `src`
+    package_dir={"": SOURCE_DIR},
+    # next line is only required if you have data files that have to be included
+    # e.g. csvs which define certain conventions etc.
+    # include_package_data=True,
+    install_requires=REQUIREMENTS,
+    extras_require=requirements_extras,
+    cmdclass=cmdclass,
+)
