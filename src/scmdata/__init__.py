786ff6849e5e78dac2f401f4b471390e9bbb109a
Jared Lewis <jared@jared.kiwi.nz>
2020-02-04 12:15:08 +1100

    Reorder toplevel imports

--- src/scmdata/__init__.py
+++ src/scmdata/__init__.py
@@ -1,6 +1,6 @@
-from scmdata.dataframe import ScmDataFrame, df_append  # noqa: F401
-
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from scmdata.dataframe import ScmDataFrame, df_append  # noqa: F401
