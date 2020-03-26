4d128fcfee25fc123f5e49fa2f86b8b56ea9327b
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-03-26 17:59:04 +1100

    Fix test requirements

--- setup.py
+++ setup.py
@@ -30,7 +30,12 @@ CLASSIFIERS = [
 
 REQUIREMENTS = ["numpy", "python-dateutil", "pint", "pandas"]
 REQUIREMENTS_PYAM = ["pyam-iamc>=0.3.0"]
-REQUIREMENTS_TESTS = ["codecov", "nbval", "pytest-cov", "pytest>=5.0.0"]
+REQUIREMENTS_TESTS = [
+    "codecov",
+    "nbval",
+    "pytest-cov",
+    "pytest>=5.0.0",
+] + REQUIREMENTS_PYAM
 REQUIREMENTS_DOCS = ["sphinx>=1.4,<2.1", "sphinx_rtd_theme"]
 REQUIREMENTS_DEPLOY = ["twine>=1.11.0", "setuptools>=38.6.0", "wheel>=0.31.0"]
 
