9860f0a69ade42ff6a2799d9c63ac54b20e3cae7
Jared Lewis <jared@jared.kiwi.nz>
2020-04-09 11:27:47 +1000

    add netCDF4 as optional dep

--- setup.py
+++ setup.py
@@ -36,13 +36,14 @@ REQUIREMENTS = [
     "python-dateutil",
     "xarray",
 ]
+REQUIREMENTS_OPTIONAL = ["netCDF4"]
 REQUIREMENTS_PYAM = ["pyam-iamc>=0.3.0"]
 REQUIREMENTS_TESTS = [
     "codecov",
     "nbval",
     "pytest-cov",
     "pytest>=5.0.0",
-] + REQUIREMENTS_PYAM
+] + REQUIREMENTS_PYAM + REQUIREMENTS_OPTIONAL
 REQUIREMENTS_DOCS = ["sphinx>=1.4,<2.1", "sphinx_rtd_theme"]
 REQUIREMENTS_DEPLOY = ["twine>=1.11.0", "setuptools>=38.6.0", "wheel>=0.31.0"]
 
@@ -58,8 +59,8 @@ REQUIREMENTS_DEV = [
         "pydocstyle",
         "pylint",
         "scipy",
-        "netCDF4",
     ],
+    *REQUIREMENTS_OPTIONAL,
     *REQUIREMENTS_PYAM,
     *REQUIREMENTS_TESTS,
     *REQUIREMENTS_DOCS,
