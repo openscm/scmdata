fa9810a3ab49018ab1a103fec9bd054bbd531276
Jared Lewis <jared@jared.kiwi.nz>
2020-05-22 16:58:44 +1000

    Fix docs

--- src/scmdata/run.py
+++ src/scmdata/run.py
@@ -506,11 +506,10 @@ class ScmRun:  # pylint: disable=too-many-public-methods
 
     def drop_meta(self, columns: Union[list, str], inplace: bool = True):
         """
-        Drops columns out of the Run
+        Drop columns out of the Run
 
         Notes
         -----
-
         If this operation is not performed inplace, the current object is deep copied. Any changes to the :obj:`Timeseries`s of
         the returned object will not be reflected in the previous object
 
