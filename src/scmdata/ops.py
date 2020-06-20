2e233e6a1eb519c2c673f6eea3fd27ffc5344623
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-06-20 18:43:56 +1000

    Add multiply and divide

--- src/scmdata/ops.py
+++ src/scmdata/ops.py
@@ -59,6 +59,54 @@ def add(self, other, op_cols):
     return type(self)(out)
 
 
+def multiply(self, other, op_cols):
+    """
+    Multiply values
+
+    Parameters
+    ----------
+    other : :obj:`ScmRun`
+        :obj:`ScmRun` containing data to multiply
+
+    op_cols : dict of str: str
+        Dictionary containing the columns to drop before multiplying as the keys and the value those columns should hold in the output as the values. For example, if we have ``op_cols={"variable": "Emissions|CO2 - Emissions|CO2|Fossil"}`` then the multiplication will be performed with an index that uses all columns except the "variable" column and the output will have a "variable" column with the value "Emissions|CO2 - Emissions|CO2|Fossil".
+
+    Returns
+    -------
+    :obj:`ScmRun`
+        Product of ``self`` and ``other``, using ``op_cols`` to define the columns which should be dropped before the data is aligned and to define the value of these columns in the output.
+    """
+    out = _prep_for_op(self, op_cols) * _prep_for_op(other, op_cols)
+
+    out = _set_op_values(out, op_cols)
+
+    return type(self)(out)
+
+
+def divide(self, other, op_cols):
+    """
+    Divide values (self / other)
+
+    Parameters
+    ----------
+    other : :obj:`ScmRun`
+        :obj:`ScmRun` containing data to divide
+
+    op_cols : dict of str: str
+        Dictionary containing the columns to drop before dividing as the keys and the value those columns should hold in the output as the values. For example, if we have ``op_cols={"variable": "Emissions|CO2 - Emissions|CO2|Fossil"}`` then the division will be performed with an index that uses all columns except the "variable" column and the output will have a "variable" column with the value "Emissions|CO2 - Emissions|CO2|Fossil".
+
+    Returns
+    -------
+    :obj:`ScmRun`
+        Quotient of ``self`` and ``other``, using ``op_cols`` to define the columns which should be dropped before the data is aligned and to define the value of these columns in the output.
+    """
+    out = _prep_for_op(self, op_cols) / _prep_for_op(other, op_cols)
+
+    out = _set_op_values(out, op_cols)
+
+    return type(self)(out)
+
+
 def inject_ops_methods(cls):
     """
     Inject the operation methods
@@ -71,9 +119,8 @@ def inject_ops_methods(cls):
     methods = [
         ("subtract", subtract),
         ("add", add),
-        # TODO: add these
-        # ("multiply", multiply),
-        # ("divide", divide),
+        ("multiply", multiply),
+        ("divide", divide),
     ]
 
     for name, f in methods:
--- tests/unit/test_ops.py
+++ tests/unit/test_ops.py
@@ -42,3 +42,41 @@ def test_add_single_timeseries():
     exp = ScmRun(exp_ts)
 
     assert_scmdf_almost_equal(res, exp, allow_unordered=True, check_ts_names=False)
+
+
+def test_multiply_single_timeseries():
+    common_cols = {"scenario": "scen", "model": "mod", "unit": "GtC / yr", "region": "World"}
+
+    base = ScmRun(data=[1, 2, 3], index=[1, 2, 3], columns={"variable": "Emissions|CO2", **common_cols})
+    other = ScmRun(data=[3, 2, 1], index=[1, 2, 3], columns={"variable": "Emissions|CO2|Fossil", **common_cols})
+
+    res = base.multiply(other, op_cols={"variable": "Emissions|CO2|AFOLU"})
+
+    base_ts = base.timeseries().reset_index("variable", drop=True)
+    other_ts = other.timeseries().reset_index("variable", drop=True)
+
+    exp_ts = base_ts * other_ts
+    exp_ts["variable"] = "Emissions|CO2|AFOLU"
+
+    exp = ScmRun(exp_ts)
+
+    assert_scmdf_almost_equal(res, exp, allow_unordered=True, check_ts_names=False)
+
+
+def test_divide_single_timeseries():
+    common_cols = {"scenario": "scen", "model": "mod", "unit": "GtC / yr", "region": "World"}
+
+    base = ScmRun(data=[1, 2, 3], index=[1, 2, 3], columns={"variable": "Emissions|CO2", **common_cols})
+    other = ScmRun(data=[3, 2, 1], index=[1, 2, 3], columns={"variable": "Emissions|CO2|Fossil", **common_cols})
+
+    res = base.divide(other, op_cols={"variable": "Emissions|CO2|AFOLU"})
+
+    base_ts = base.timeseries().reset_index("variable", drop=True)
+    other_ts = other.timeseries().reset_index("variable", drop=True)
+
+    exp_ts = base_ts / other_ts
+    exp_ts["variable"] = "Emissions|CO2|AFOLU"
+
+    exp = ScmRun(exp_ts)
+
+    assert_scmdf_almost_equal(res, exp, allow_unordered=True, check_ts_names=False)
