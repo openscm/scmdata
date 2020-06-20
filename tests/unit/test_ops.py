dba0a308306650ee9e4b8c606d7c3554a77aa807
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-06-20 19:12:50 +1000

    Update testing of unit handling in ops

--- tests/unit/test_ops.py
+++ tests/unit/test_ops.py
@@ -2,6 +2,8 @@
 # - operations align properly (i.e. use more than one timeseries in tests)
 # - what happens if the subtraction doesn't align properly (either some rows in one ScmRun that aren't in the other or metadata is not unique once the ``op_cols`` are removed)
 # - multiple ``op_cols`` also works
+# - can do e.g. population per capita with right units
+import numpy as np
 import pytest
 
 from scmdata.run import ScmRun
@@ -12,9 +14,16 @@ def get_ts(data, index, **kwargs):
     return ScmRun(data=data, index=index, columns=kwargs)
 
 
-def get_single_ts(data=[1, 2, 3], index=[1, 2, 3], variable="Emissions|CO2",
-    scenario="scen", model="mod", unit="GtC / yr",
-    region="World", **kwargs):
+def get_single_ts(
+    data=[1, 2, 3],
+    index=[1, 2, 3],
+    variable="Emissions|CO2",
+    scenario="scen",
+    model="mod",
+    unit="GtC / yr",
+    region="World",
+    **kwargs
+):
 
     return get_ts(
         data=data,
@@ -28,10 +37,16 @@ def get_single_ts(data=[1, 2, 3], index=[1, 2, 3], variable="Emissions|CO2",
     )
 
 
-def get_multiple_ts(data=[1, 2, 3], index=[1, 2, 3], variable="Emissions|CO2",
-    scenario="scen", model="mod", unit="GtC / yr",
-    region="World", **kwargs):
-
+def get_multiple_ts(
+    data=np.array([[1, 2, 3], [10, 20, 30]]).T,
+    index=[2020, 2030, 2040],
+    variable=["Emissions|CO2", "Emissions|CH4"],
+    scenario="scen",
+    model="mod",
+    unit=["GtC / yr", "MtCH4 / yr"],
+    region="World",
+    **kwargs
+):
     return get_ts(
         data=data,
         index=index,
@@ -51,17 +66,17 @@ def base_single_scmrun():
 
 @pytest.fixture
 def other_single_scmrun():
-    return get_single_ts(variable="Emissions|CO2|Fossil")
+    return get_single_ts(data=[-1, 0, 5], variable="Emissions|CO2|Fossil")
 
 
 @pytest.fixture
 def base_multiple_scmrun():
-    return get_single_ts(variable="Emissions|CO2")
+    return get_multiple_ts(scenario="Scenario A")
 
 
 @pytest.fixture
 def other_multiple_scmrun():
-    return get_single_ts(variable="Emissions|CO2|Fossil")
+    return get_multiple_ts(data=np.array([[-1, 0, 3.2], [11.1, 20, 32.3]]).T, scenario="Scenario B")
 
 
 OPS_MARK = pytest.mark.parametrize("op", ("add", "subtract", "multiply", "divide"))
@@ -86,20 +101,23 @@ def perform_op(base, other, op, reset_index):
     else:
         raise NotImplementedError(op)
 
-    return exp_ts
+    return exp_ts.reset_index()
 
 
 @OPS_MARK
 def test_single_timeseries(op, base_single_scmrun, other_single_scmrun):
-    res = getattr(base_single_scmrun, op)(other_single_scmrun, op_cols={"variable": "Emissions|CO2|AFOLU"})
-
-    exp_ts = perform_op(
-        base_single_scmrun,
-        other_single_scmrun,
-        op,
-        "variable"
+    res = getattr(base_single_scmrun, op)(
+        other_single_scmrun, op_cols={"variable": "Emissions|CO2|AFOLU"}
     )
+
+    exp_ts = perform_op(base_single_scmrun, other_single_scmrun, op, "variable")
     exp_ts["variable"] = "Emissions|CO2|AFOLU"
+
+    if op == "multiply":
+        exp_ts["unit"] = "{}**2".format(base_single_scmrun.get_unique_meta("unit", no_duplicates=True))
+
+    if op == "divide":
+        exp_ts["unit"] = "dimensionless"
 
     exp = ScmRun(exp_ts)
 
@@ -108,16 +126,12 @@ def test_single_timeseries(op, base_single_scmrun, other_single_scmrun):
 
 @OPS_MARK
 def test_multiple_timeseries(op, base_multiple_scmrun, other_multiple_scmrun):
-    res = getattr(base_multiple_scmrun, op)(other_multiple_scmrun, op_cols={"scenario": "one to two"})
-
-    exp_ts = perform_op(
-        base_multiple_scmrun,
-        other_multiple_scmrun,
-        op,
-        "scenario"
+    res = getattr(base_multiple_scmrun, op)(
+        other_multiple_scmrun, op_cols={"scenario": "A to B"}
     )
-    exp_ts["scenario"] = "one to two"
+
+    exp_ts = perform_op(base_multiple_scmrun, other_multiple_scmrun, op, "scenario")
+    exp_ts["scenario"] = "A to B"
 
     exp = ScmRun(exp_ts)
-
     assert_scmdf_almost_equal(res, exp, allow_unordered=True, check_ts_names=False)
