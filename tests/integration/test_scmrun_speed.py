f3a74c9d57d907485d917c102002cfba7a02471f
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-09-03 17:52:04 +1000

    Add speed tests

---
+++ tests/integration/test_scmrun_speed.py
@@ -0,0 +1,143 @@
+import random
+
+import numpy as np
+import pytest
+
+import scmdata
+
+
+@pytest.fixture(params=[10, 10 ** 2, 10 ** 3, 10 ** 3.5, 10 ** 4, 10 ** 4.5])
+def big_scmrun(request):
+    length = int(request.param)
+    t_steps = 750
+    variables = [
+        "Surface Air Temperature Change",
+        "Surface Air Ocean Blended Temperature Change",
+        "Effective Radiative Forcing",
+        "Atmospheric Concentrations|CO2",
+    ]
+    scenarios = ["ssp119", "ssp245", "ssp434", "ssp460", "ssp126", "esm-1pctCo2"]
+    regions = [
+        "World",
+        "World|R5.2ASIA",
+        "World|R5.2LAM",
+        "World|R5.2MAF",
+        "World|R5.2REF",
+        "World|R5.2OECD",
+        "World|Bunkers",
+    ]
+    climate_models = ["MAGICC7", "MAGICC6", "MAGICC5", "Bill", "Bloggs"]
+    to_squash = ["aa", "ab", "ba", "bb"]
+
+    return scmdata.ScmRun(
+        np.random.random((length, t_steps)).T,
+        index=range(1750, 1750 + t_steps),
+        columns={
+            "model": "unspecified",
+            "variable": random.choices(variables, k=length),
+            "unit": "unknown",
+            "scenario": random.choices(scenarios, k=length),
+            "region": random.choices(regions, k=length),
+            "climate_model": random.choices(climate_models, k=length),
+            "to_squash": random.choices(to_squash, k=length),
+            "ensemble_member": range(length),
+        },
+    )
+
+
+def test_recreate_from_timeseries(benchmark, big_scmrun):
+    def recreate():
+        return scmdata.ScmRun(big_scmrun.timeseries())
+
+    benchmark.pedantic(recreate, iterations=1, rounds=2)
+
+
+def test_filter(benchmark, big_scmrun):
+    def variable_filter():
+        return big_scmrun.filter(
+            variable="Effective Radiative Forcing", year=range(1850, 1910 + 1)
+        )
+
+    result = benchmark.pedantic(variable_filter, iterations=1, rounds=2)
+    assert (
+        result.get_unique_meta("variable", no_duplicates=True)
+        == "Effective Radiative Forcing"
+    )
+    assert result["year"].iloc[0] == 1850
+    assert result["year"].iloc[-1] == 1910
+
+
+def test_get_unique_meta(benchmark, big_scmrun):
+    def retrieve_unique_meta():
+        return big_scmrun.get_unique_meta("variable")
+
+    result = benchmark.pedantic(retrieve_unique_meta, iterations=1, rounds=2)
+    assert len(result) > 0
+
+
+def test_append(benchmark, big_scmrun):
+    def setup():
+        other = big_scmrun.copy()
+        other["ensemble_member"] += max(other["ensemble_member"]) + 1
+
+        return (), {"other": other}
+
+    def append_runs(other):
+        return big_scmrun.append(other)
+
+    res = benchmark.pedantic(append_runs, setup=setup, iterations=1, rounds=2)
+    assert len(res) == 2 * len(big_scmrun)
+
+
+def test_append_other_time_axis(benchmark, big_scmrun):
+    def setup():
+        other = big_scmrun.timeseries(time_axis="year")
+        other.columns = other.columns.map(lambda x: x + max(other.columns) + 1)
+        other = scmdata.ScmRun(other)
+        other["ensemble_member"] += max(other["ensemble_member"]) + 1
+
+        return (), {"other": other}
+
+    def append_runs_different_times(other):
+        return scmdata.run_append([big_scmrun, other])
+
+    res = benchmark.pedantic(
+        append_runs_different_times, setup=setup, iterations=1, rounds=2
+    )
+    assert len(res) == 2 * len(big_scmrun)
+    assert res.shape[1] == 2 * big_scmrun.shape[1]
+
+
+def test_set_meta_single(benchmark, big_scmrun):
+    def set_meta():
+        big_scmrun["model"] = "new value"
+
+        return big_scmrun
+
+    res = benchmark.pedantic(set_meta, iterations=1, rounds=2)
+    assert res.get_unique_meta("model", no_duplicates=True) == "new value"
+
+
+def test_set_meta_all_with_apply(benchmark, big_scmrun):
+    def set_meta():
+        big_scmrun["variable"] = big_scmrun["variable"].apply(
+            lambda x: "New|{}".format(x)
+        )
+
+        return big_scmrun
+
+    res = benchmark.pedantic(set_meta, iterations=1, rounds=2)
+    assert res["variable"].str.startswith("New|").all()
+
+
+def test_set_meta_reduce_uniqueness(benchmark, big_scmrun):
+    def set_meta():
+        big_scmrun["to_squash"] = big_scmrun["to_squash"].apply(
+            lambda x: "a" if x.startswith("a") else "b"
+        )
+
+        return big_scmrun
+
+    original_set = big_scmrun.get_unique_meta("to_squash")
+    res = benchmark.pedantic(set_meta, iterations=1, rounds=2)
+    assert not any([v in original_set for v in res.get_unique_meta("to_squash")])
