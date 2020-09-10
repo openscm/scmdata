611e4d86b2cd22a890979f48b4d04500e835c884
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-09-10 18:06:21 +1000

    Add changes

--- src/scmdata/run.py
+++ src/scmdata/run.py
@@ -1614,26 +1614,30 @@ class ScmRun:  # pylint: disable=too-many-public-methods
         else:
             ret = self.copy()
 
-        to_convert = ret.filter(**kwargs)
-        to_not_convert = ret.filter(**kwargs, keep=False, log_if_empty=False)
+        already_correct_unit = ret.filter(unit=unit, log_if_empty=False)
+        if (
+            "unit_context" in already_correct_unit.meta_attributes
+            and not already_correct_unit.empty
+        ):
+            self._check_unit_context(already_correct_unit, context)
+
+        not_correct_unit = ret.filter(unit=unit, keep=False, log_if_empty=False)
+        if not_correct_unit.empty:
+            to_convert = not_correct_unit
+            to_not_convert = already_correct_unit
+        else:
+            to_convert = not_correct_unit.filter(**kwargs)
+            to_not_convert = run_append(
+                [
+                    not_correct_unit.filter(**kwargs, keep=False, log_if_empty=False),
+                    already_correct_unit,
+                ]
+            )
 
         if "unit_context" in to_convert.meta_attributes:
-            unit_context = to_convert.get_unique_meta("unit_context")
-            # check if contexts don't match, unless the context is nan
-            non_matching_contexts = len(unit_context) > 1 or unit_context[0] != context
-            if isinstance(unit_context[0], float):
-                non_matching_contexts &= not np.isnan(unit_context[0])
+            self._check_unit_context(to_convert, context)
 
-            if non_matching_contexts:
-                raise ValueError(
-                    "Existing unit conversion context(s), `{}`, doesn't match input "
-                    "context, `{}`, drop `unit_context` metadata before doing "
-                    "conversion".format(unit_context, context)
-                )
-
-            to_convert["unit_context"] = context
-
-        elif context is not None:
+        if context is not None:
             to_convert["unit_context"] = context
 
         if "unit_context" not in to_not_convert.meta_attributes and context is not None:
@@ -1648,11 +1652,28 @@ class ScmRun:  # pylint: disable=too-many-public-methods
 
             return group
 
-        ret = to_convert.groupby("unit").map(apply_units)
+        ret = to_convert
+        if not to_convert.empty:
+            ret = ret.groupby("unit").map(apply_units)
 
         ret = run_append([ret, to_not_convert], inplace=inplace)
         if not inplace:
             return ret
+
+    @staticmethod
+    def _check_unit_context(dat, context):
+        unit_context = dat.get_unique_meta("unit_context")
+        # check if contexts don't match, unless the context is nan
+        non_matching_contexts = len(unit_context) > 1 or unit_context[0] != context
+        if isinstance(unit_context[0], float):
+            non_matching_contexts &= not np.isnan(unit_context[0])
+
+        if non_matching_contexts:
+            raise ValueError(
+                "Existing unit conversion context(s), `{}`, doesn't match input "
+                "context, `{}`, drop `unit_context` metadata before doing "
+                "conversion".format(unit_context, context)
+            )
 
     def relative_to_ref_period_mean(self, append_str=None, **kwargs):
         """
