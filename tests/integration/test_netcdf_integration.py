c080f93cd738890c188aa7a9189f6b1025e37ac1
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-02-15 16:06:55 +1100

    Pass tests

--- tests/integration/test_netcdf_integration.py
+++ tests/integration/test_netcdf_integration.py
@@ -4,7 +4,9 @@ from os.path import join
 import numpy as np
 import xarray as xr
 
+from scmdata import ScmRun
 from scmdata.netcdf import run_to_nc
+from scmdata.testing import assert_scmdf_almost_equal
 
 
 def test_run_to_nc_read_with_xarray(scm_run):
@@ -17,3 +19,9 @@ def test_run_to_nc_read_with_xarray(scm_run):
     # check that time is read back in as datetime
     assert xr_dataset["time"].dtype == "<M8[ns]"
     assert not isinstance(xr_dataset["time"].values[0], np.float64)
+
+
+def test_read_legacy_datetimes_nc(scm_run, test_data_path):
+    old_datetimes_run = ScmRun.from_nc(join(test_data_path, "legacy_datetimes.nc"))
+
+    assert_scmdf_almost_equal(old_datetimes_run, scm_run, check_ts_names=False)
