571d70c9970a12489fa873b0829ad9f75475f1d1
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-09-08 15:29:06 +1000

    Add new module

---
+++ src/scmdata/processing.py
@@ -0,0 +1,8 @@
+"""
+Miscellaneous functions for processing :class:`scmdata.ScmRun`
+
+These functions are intended to be able to be used directly with
+:meth:`scmdata.ScmRun.process_over`.
+"""
+# exceedance probabilities
+# crossing times
