26d5aa5b3c2ef679ee0dbb3b210a90afac3f031d
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-09-10 13:06:26 +1000

    Add multi-model test of crossing times

--- tests/integration/test_processing.py
+++ tests/integration/test_processing.py
@@ -14,7 +14,7 @@ def test_processing_scm_df():
     data = np.array(
         [
             [1, 1.1, 1.2, 1.1],
-            [1.1, 1.2, 1.3, 1.3],
+            [1.1, 1.2, 1.3, 1.41],
             [1.3, 1.4, 1.5, 1.6],
             [1.3, 1.5, 1.6, 1.2],
             [1.48, 1.51, 1.72, 1.56],
@@ -34,6 +34,26 @@ def test_processing_scm_df():
         index=[2005, 2006, 2007, 2100],
     )
 
+
+@pytest.fixture()
+def test_processing_scm_df_multi_climate_model(test_processing_scm_df):
+    other = test_processing_scm_df + 0.1
+    other["climate_model"] = "z_model"
+
+    return test_processing_scm_df.append(other)
+
+
+crossing_times_year_conversions = pytest.mark.parametrize(
+    "return_year,conv_to_year", ((None, True), (True, True), (False, False),)
+)
+
+
+def _get_calculate_crossing_times_call_kwargs(return_year):
+    call_kwargs = {}
+    if return_year is not None:
+        call_kwargs["return_year"] = return_year
+
+    return call_kwargs
 
 @pytest.mark.parametrize(
     "threshold,exp_vals",
@@ -61,16 +81,11 @@ def test_processing_scm_df():
         (2.0, [np.nan, np.nan, np.nan, np.nan, np.nan]),
     ),
 )
-@pytest.mark.parametrize(
-    "return_year,conv_to_year", ((None, True), (True, True), (False, False),)
-)
+@crossing_times_year_conversions
 def test_crossing_times(
     threshold, exp_vals, return_year, conv_to_year, test_processing_scm_df
 ):
-    call_kwargs = {}
-    if return_year is not None:
-        call_kwargs["return_year"] = return_year
-
+    call_kwargs = _get_calculate_crossing_times_call_kwargs(return_year)
     res = scmdata.processing.calculate_crossing_times(
         test_processing_scm_df, threshold=threshold, **call_kwargs,
     )
@@ -80,6 +95,45 @@ def test_crossing_times(
     else:
         exp_vals = [pd.NaT if pd.isnull(v) else v for v in exp_vals]
 
-    exp = pd.Series(exp_vals, pd.MultiIndex.from_frame(test_processing_scm_df.meta),)
+    exp = pd.Series(exp_vals, pd.MultiIndex.from_frame(test_processing_scm_df.meta))
 
     pdt.assert_series_equal(res, exp)
+
+
+@crossing_times_year_conversions
+def test_crossing_times_multi_climate_model(
+    return_year, conv_to_year, test_processing_scm_df_multi_climate_model
+):
+    call_kwargs = _get_calculate_crossing_times_call_kwargs(return_year)
+
+    threshold = 1.5
+    exp_vals = [
+        # a_model
+        np.nan,
+        np.nan,
+        dt.datetime(2100, 1, 1),
+        dt.datetime(2007, 1, 1),
+        dt.datetime(2006, 1, 1),
+        # z_model
+        np.nan,
+        dt.datetime(2100, 1, 1),
+        dt.datetime(2007, 1, 1),
+        dt.datetime(2006, 1, 1),
+        dt.datetime(2005, 1, 1),
+    ]
+
+    res = scmdata.processing.calculate_crossing_times(
+        test_processing_scm_df_multi_climate_model,
+        threshold=threshold,
+        **call_kwargs,
+    )
+
+    if conv_to_year:
+        exp_vals = [v if pd.isnull(v) else v.year for v in exp_vals]
+    else:
+        exp_vals = [pd.NaT if pd.isnull(v) else v for v in exp_vals]
+
+    exp = pd.Series(exp_vals, pd.MultiIndex.from_frame(test_processing_scm_df_multi_climate_model.meta))
+
+    pdt.assert_series_equal(res, exp)
+
