46bd8641aa9d62e4d2fb3e1ee4c5bedb650a8e24
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2022-03-15 14:46:10 +1100

    Run notebooks and tweak safe handling

--- src/scmdata/database.py
+++ src/scmdata/database.py
@@ -57,10 +57,10 @@ def _check_is_subdir(root, d):
 
 def _get_safe_filename(inp):
     def safe_char(c):
-        if c.isalnum() or c in f"-{os.sep}*_.":
+        if c.isalnum() or c in f"-*_." or c == os.sep:
             return c
-        if __is_windows__ and c in [":"]:
-            return c
+        # if __is_windows__ and c in [":"]:
+        #     return c
 
         return "-"
 
