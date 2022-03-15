9aa624f494fb49fd07855174de8a0443b2526de0
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2022-03-15 17:48:56 +1100

    Remove versioneer stuff

--- src/scmdata/__init__.py
+++ src/scmdata/__init__.py
@@ -1,10 +1,20 @@
 """
-scmdata, simple data handling for simple climate model data
+scmdata, simple data handling for simple climate model data.
+
+See README and docs for more info.
 """
-from ._version import get_versions  # isort:skip
+from .run import run  # noqa: F401
 
-__version__ = get_versions()["version"]
-del get_versions
+try:
+    from importlib.metadata import version as _version
+except ImportError:
+    # no recourse if the fallback isn't there either...
+    from importlib_metadata import version as _version
 
+try:
+    __version__ = _version("openscm_runner")
+except Exception:  # pylint: disable=broad-except  # pragma: no cover
+    # Local copy, not installed with setuptools
+    __version__ = "unknown"
 
 from scmdata.run import ScmRun, run_append  # noqa: F401, E402
