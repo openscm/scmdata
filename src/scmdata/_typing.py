d22f4cbc1d40dce317021695b997c673eed91d31
Jared Lewis <jared@jared.kiwi.nz>
2023-05-17 09:09:58 +0200

    chore: Revert as it didn't work in python38

--- src/scmdata/_typing.py
+++ src/scmdata/_typing.py
@@ -3,12 +3,12 @@ Commonly used typehints
 """
 from __future__ import annotations
 
-from os import PathLike
+from os import PathLike  # noqa
 from typing import Callable, Dict, Union
 
 import pandas as pd
 
-FilePath = Union[str, PathLike[str]]
+FilePath = Union[str, "PathLike[str]"]
 MetadataValue = Union[str, int, float]
 MetadataType = Dict[str, MetadataValue]
 ApplyCallable = Callable[[pd.DataFrame], Union[pd.DataFrame, pd.Series, float]]
