3188641a4e30fe1a61f6beff803dda5ec976bae1
Jared Lewis <jared@jared.kiwi.nz>
2023-08-22 18:03:14 +1000

    chore: Fix up CI

--- src/scmdata/_typing.py
+++ src/scmdata/_typing.py
@@ -6,12 +6,9 @@ from __future__ import annotations
 from os import PathLike  # noqa
 from typing import Callable, Dict, Union
 
-import numpy as np
 import pandas as pd
-from numpy.typing import NDArray
 
 FilePath = Union[str, "PathLike[str]"]
 MetadataValue = Union[str, int, float]
 MetadataType = Dict[str, MetadataValue]
 ApplyCallable = Callable[[pd.DataFrame], Union[pd.DataFrame, pd.Series, float]]
-FloatArray = NDArray[np.float_]
